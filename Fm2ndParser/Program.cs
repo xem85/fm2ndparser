0ac0ef5d2c20688e06ec27abbdf69bd81f3cd491
Xem85 <290998366+xem85@users.noreply.github.com>
2026-07-21 15:34:52 +0200

    allow empty verb (select parse by default

--- Fm2ndParser/Program.cs
+++ Fm2ndParser/Program.cs
@@ -2,6 +2,7 @@
 using CommandLine.Text;
 using Fm2ndParser.Common;
 using Fm2ndParser.Parsers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,25 +20,37 @@ namespace Fm2ndParser
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            var parsed = Parser.Default.ParseArguments<ParseOptions, GenerateOptions>(args)
-                .MapResult(
+            
+            var knownVerbs = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "parse",
+                "generate"
+            };
 
-                (ParseOptions opts) => new ParseCommand(
-                   opts.InputFiles.Single(),
-                   opts.CleanUp,
-                   opts.NewFiles,
-                   opts.ExportResources
-                ).Execute(),
-
-                (GenerateOptions opts) => new GenerateCommand(opts.InputFiles.Single()).Execute(),
-
-                errs => HandleParseError(errs)
-            );
-            await parsed;
+            if (args.Length > 0 && !args[0].StartsWith("-") && !knownVerbs.Contains(args[0]))
+            {
+                // select the default verb "parse" if the first argument is not a known verb
+                args = new[] { "parse" }.Concat(args).ToArray();
             }
 
+            var parsed = Parser.Default.ParseArguments<ParseOptions, GenerateOptions>(args)
+                .MapResult(
+                    (ParseOptions opts) => new ParseCommand(
+                        opts.InputFiles.Single(),
+                        opts.CleanUp,
+                        opts.NewFiles,
+                        opts.ExportResources
+                    ).Execute(),
+
+                    (GenerateOptions opts) => new GenerateCommand(opts.InputFiles.Single()).Execute(),
+                    errs => HandleParseError(errs)
+                );
+
+            await parsed;
+        }
+
         private static Task HandleParseError(IEnumerable<Error> obj)
-            {
+        {
             //Console.WriteLine("You must specify a .player path as argument");
             return Task.CompletedTask;
         }
